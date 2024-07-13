d39bfe044884a824a636c0949f10b3db50532052
JesMansilla <jesicavmansilla@gmail.com>
2024-07-13 14:17:49 -0300

    first commit

---
+++ listadoEliminar.js
@@ -0,0 +1,50 @@
+const URL = "https://jesicamansilla.pythonanywhere.com/" 
+        
+        
+        function obtenerProductos() {
+            fetch(URL + 'productos') 
+                .then(response => {
+
+                    if (response.ok) { return response.json(); }
+                })
+               
+                .then(data => {
+                    const productosTable = document.getElementById('productos-table').getElementsByTagName('tbody')[0];
+                    productosTable.innerHTML = ''; 
+                    data.forEach(producto => {
+                        const row = productosTable.insertRow();
+                        row.innerHTML = `
+                            <td>${producto.codigo}</td>
+                            <td>${producto.descripcion}</td>
+                            <td>${producto.cantidad}</td>
+                            <td align="right">${producto.precio}</td>
+                            <td><button onclick="eliminarProducto('${producto.codigo}')">Eliminar</button></td>
+                        `;
+                    });
+                })
+
+                .catch(error => {
+                    console.log('Error:', error);
+                    alert('Error al obtener los productos.');
+                });
+        }
+
+
+        function eliminarProducto(codigo) {
+            if (confirm('¿Estás seguro de que quieres eliminar este producto?')) {
+                fetch(URL + `productos/${codigo}`, { method: 'DELETE' })
+                    .then(response => {
+                        if (response.ok) {
+                            obtenerProductos(); 
+                            alert('Producto eliminado correctamente.');
+                        }
+                    })
+                    
+                    .catch(error => {
+                        alert(error.message);
+                    });
+            }
+        }
+
+       
+        document.addEventListener('DOMContentLoaded', obtenerProductos);
